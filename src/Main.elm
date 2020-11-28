9c8c094751cb566144ffbdd1c973e56014988ecc
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-11-28 22:46:13 +0100

    adds a few placeholder boxes to get an idea how it will look like

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, h1, h2, span, text)
+import Html exposing (Html, button, div, h1, h2, h3, span, text)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
@@ -32,10 +32,23 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "container p-10 dark:text-white" ]
+    div [ class "container p-10 mx-auto dark:text-white" ]
         [ h1 [ class "text-3xl font-black tracking-tight pb-4" ] [ text "Covid19 in Hamburg" ]
-        , h2 [ class "text-3x1 font-extrabold tracking-tight sm:text-4x1 text-indigo-600" ] [ text "New Infections" ]
-        , h2 [ class "text-3x1 text-gray-600 font-light tracking-tight sm:text-4x1" ] [ text "Today" ]
+        , h2 [ class "text-3xl font-extrabold tracking-tight sm:text-4x1 text-red-500 pb-1" ] [ text "New Infections" ]
+        , div
+            [ class "grid grid-cols-1 md:grid-cols-3 gap-4 uppercase text-2xl md:text-xl" ]
+            [ viewInfected "today"
+            , viewInfected "last seven days"
+            , viewInfected "since outbreak"
+            ]
+        ]
+
+
+viewInfected : String -> Html Msg
+viewInfected headline =
+    div []
+        [ h3 [] [ text headline ]
+        , div [ class "bg-red-500 text-center text-4xl text-white flex items-center justify-center font-black rounded-lg p-16" ] [ text "+356" ]
         ]
 
 
