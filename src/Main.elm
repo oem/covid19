6c1bad472ea7225958450323cc2656b16ee6339e
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-11-29 15:34:11 +0100

    displays the latest total number of infected from the model rearranges the code a bit

--- src/Main.elm
+++ src/Main.elm
@@ -6,13 +6,14 @@ import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
+
+-- MODEL
+
+
 type alias Model =
-    { count : Int }
-
-
-initialModel : Model
-initialModel =
-    { count = 0 }
+    { new : List Int
+    , total : List Int
+    }
 
 
 type Msg
@@ -20,18 +21,56 @@ type Msg
     | Decrement
 
 
+
+-- INIT
+
+
+initialModel : Model
+initialModel =
+    { new = [ 104, 300, 252, 360, 363, 392, 237, 172, 433, 362, 659, 246 ]
+    , total = [ 24710, 24606, 24306, 24054, 23694 ]
+    }
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
+
+
+
+-- UPDATE
+
+
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            { model | count = model.count + 1 }
+            model
 
         Decrement ->
-            { model | count = model.count - 1 }
+            model
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
+    let
+        allInfected : String
+        allInfected =
+            case model.total of
+                latest :: older ->
+                    String.fromInt latest
+
+                [] ->
+                    ""
+    in
     div [ class "container p-2 md:p-4 mx-auto max-w-6xl" ]
         [ h1 [ class "text-3xl font-black tracking-tight pb-4 pt-14" ] [ text "Covid19 in Hamburg" ]
         , h2 [ class "text-2xl font-extrabold tracking-tight sm:text-4x1 text-red-500 pb-1" ] [ text "New Infections" ]
@@ -39,7 +78,7 @@ view model =
             [ class "grid grid-cols-1 md:grid-cols-3 gap-4 place-content-center font-bold uppercase text-3xl md:text-2xl" ]
             [ viewToday 356
             , viewWeek 1156
-            , viewAll 112872
+            , viewAll allInfected
             ]
         ]
 
@@ -66,19 +105,10 @@ viewWeek newCases =
         ]
 
 
-viewAll : Int -> Html Msg
-viewAll newCases =
+viewAll : String -> Html Msg
+viewAll allInfected =
     div []
         [ h3 [ class "tracking-widest" ] [ text "total" ]
         , div [ class "bg-gray-300 text-center text-4xl text-white flex items-center justify-center font-black rounded-lg h-40" ]
-            [ text <| String.fromInt newCases ]
+            [ text allInfected ]
         ]
-
-
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
