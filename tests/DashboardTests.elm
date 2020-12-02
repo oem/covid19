c37d9577ba999de2b266660b6b2fd7eb7efc9b25
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-12-02 23:19:03 +0100

    adds a few more tests to cover color code mappings for Today values

--- tests/DashboardTests.elm
+++ tests/DashboardTests.elm
@@ -5,13 +5,37 @@ import Expect exposing (Expectation)
 import Test exposing (..)
 
 
-severityClass1 : Test
-severityClass1 =
-    test "when today has no value"
+severityClassToday1 : Test
+severityClassToday1 =
+    test "produces bg-gray-300 for Today Nothing"
         (\_ -> Expect.equal "bg-gray-300" (severityClass (Today Nothing)))
 
 
-severityClass2 : Test
-severityClass2 =
+severityClassToday2 : Test
+severityClassToday2 =
     test "produces bg-red-500 for Today Just 180"
         (\_ -> Expect.equal "bg-red-500" (severityClass <| Today (Just 180)))
+
+
+severityClassToday3 : Test
+severityClassToday3 =
+    test "produces dark red gradient for Today Just 280"
+        (\_ -> Expect.equal "bg-gradient-to-b from-red-600 to-red-500" (severityClass <| Today (Just 280)))
+
+
+severityClassToday4 : Test
+severityClassToday4 =
+    test "produces bg-red-400 for Today Just 110"
+        (\_ -> Expect.equal "bg-red-400" (severityClass <| Today (Just 110)))
+
+
+severityClassToday5 : Test
+severityClassToday5 =
+    test "produces bg-purple-500 for Today Just 80"
+        (\_ -> Expect.equal "bg-purple-500" (severityClass <| Today (Just 80)))
+
+
+severityClassToday6 : Test
+severityClassToday6 =
+    test "produces bg-gray-300 for Today Just 20"
+        (\_ -> Expect.equal "bg-gray-300" (severityClass <| Today (Just 20)))
